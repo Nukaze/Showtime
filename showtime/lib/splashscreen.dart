611e172ffc2aec7108686475152b97bbbb348a91
Nukaze <nukaze4@gmail.com>
2024-03-17 17:58:18 +0700

    feat: splash screen animation
    - using async and Future for animation text and navigation
    - have to choice for animation
       1. Text typing
       2. Circular Progression

--- showtime/lib/splashscreen.dart
+++ showtime/lib/splashscreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'qr_scanner.dart';
+import 'dart:async';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -9,12 +10,33 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  static const String brandName = "Showtime";
+  String displayText = "";
+  static const int _waitingTextTime = 1000;
+  static const int _animationTime = 180;
+
+  Future<void> performBrandingAnimation() async {
+    Future.delayed(const Duration(milliseconds: _waitingTextTime), () {
+      Timer.periodic(const Duration(milliseconds: _animationTime), (timer) {
+        if (displayText == brandName) {
+          timer.cancel();
+          Future.delayed(const Duration(milliseconds: _waitingTextTime), () {
+            Navigator.pushReplacementNamed(context, '/QrScanner');
+          });
+        } else {
+          setState(() {
+            displayText = brandName.substring(0, displayText.length + 1);
+          });
+        }
+      });
+    });
+  }
+
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacementNamed(context, '/QrScanner');
-    });
+    // Start the timer to display the brand name
+    performBrandingAnimation();
   }
 
   @override
@@ -27,19 +49,24 @@ class _SplashScreenState extends State<SplashScreen> {
             fit: BoxFit.cover,
           ),
         ),
-        child: const Center(
+        child: Center(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              "Showtime",
-              style: TextStyle(
+              displayText,
+              style: const TextStyle(
                   fontSize: 56,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
                   fontFamily: 'Georgia',
                   fontStyle: FontStyle.italic),
-            )
+            ),
+            // const CircularProgressIndicator(
+            //   color: Colors.white,
+            //   strokeCap: StrokeCap.butt,
+            //   strokeWidth: 8,
+            // ),
           ],
         )),
       ),
