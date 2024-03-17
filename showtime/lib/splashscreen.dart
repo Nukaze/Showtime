b4ee6568fb93fafcec7904448047f22bc5a2f1d9
Nukaze <nukaze4@gmail.com>
2024-03-17 16:01:39 +0700

    feat: complete designed page
    - code frontend layout according to figma design
    - add text label "S" and change to "Showtime"
    - todo: perform splashscreen process

--- showtime/lib/splashscreen.dart
+++ showtime/lib/splashscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'qr_scanner.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -9,7 +10,39 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/QrScanner');
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/showtime_bg.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: const Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Showtime",
+              style: TextStyle(
+                  fontSize: 56,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Georgia',
+                  fontStyle: FontStyle.italic),
+            )
+          ],
+        )),
+      ),
+    );
   }
 }
