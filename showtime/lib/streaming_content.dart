2a1157bb5ee432cd991a70a7423dd955c68678b9
Nukaze <nukaze4@gmail.com>
2024-03-29 19:56:43 +0700

    feat: streaming_content by youtube lib embedded
    - using youtube_player_iframe for youtube embedded and receive videoId param from qr_scanner
    - todo provide metadata and display on user interface

---
+++ showtime/lib/streaming_content.dart
@@ -0,0 +1,76 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:showtime/utils.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
+
+class StreamingContent extends StatefulWidget {
+  final String videoId;
+
+  const StreamingContent({super.key, required this.videoId});
+
+  @override
+  _StreamingContentState createState() => _StreamingContentState();
+}
+
+class _StreamingContentState extends State<StreamingContent> {
+  late YoutubePlayerController _controller;
+  static const double _minute = 60;
+  static const double _requiredDurationInSeconds = _minute * 20;
+
+  late final dynamic _startTimeInSeconds;
+  late final dynamic _endTimeInSeconds;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = YoutubePlayerController(
+      params: const YoutubePlayerParams(
+        showControls: true,
+        showFullscreenButton: true,
+        mute: false,
+        color: 'red',
+        loop: false,
+        playsInline: true,
+        strictRelatedVideos: true,
+      ),
+    );
+
+    _controller.setFullScreenListener((isFullScreen) {
+      debugPrint('The player is ${isFullScreen ? 'fullscreen' : 'not fullscreen'}');
+    });
+
+    // actual production
+    // int maxStartTimeItShouldbe = _controller.metadata.duration.inSeconds.toInt() - _requiredDurationInSeconds.toInt();
+    // dev mode
+    int maxStartTimeItShouldbe = (_controller.metadata.duration.inSeconds.toInt() * 0.8).toInt();
+    _startTimeInSeconds = randomNumber(0, _controller.metadata.duration.inSeconds.toInt());
+    _endTimeInSeconds = _startTimeInSeconds + _requiredDurationInSeconds;
+    _controller.loadVideoById(
+      videoId: widget.videoId,
+      startSeconds: _startTimeInSeconds,
+      endSeconds: _endTimeInSeconds,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: YoutubePlayerControllerProvider(
+        controller: _controller,
+        child: YoutubePlayer(
+          controller: _controller,
+          aspectRatio: 16 / 9,
+          backgroundColor: Colors.red,
+        ),
+      ),
+    );
+  }
+}
