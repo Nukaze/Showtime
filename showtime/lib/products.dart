42b6fc5d6adb75223eedd3256145305123d2a946
Nukaze <nukaze4@gmail.com>
2024-04-22 18:50:30 +0700

    test: add double.infinity

--- showtime/lib/products.dart
+++ showtime/lib/products.dart
@@ -1,6 +1,6 @@
 class Product {
   late String name;
-  late int price;
+  late dynamic price;
   late String imageUrl;
 
   Product(this.name, this.price, this.imageUrl);
@@ -10,5 +10,15 @@ List<Product> ProductList = [
   Product("Eat set Mpass", 150, "assets/images/popcorn_mpass.jpg"),
   Product("Barbie Popcorn", 450, "assets/images/popcorn_barbie.jpg"),
   Product("Lorem ipsom", 3000, ""),
-  Product("Eiei za", 99, ""),
+  Product("Flutter Developer", double.infinity, ""),
+  Product("Lorem ipsom", 3000, ""),
+  Product("Flutter Developer", double.infinity, ""),
+  Product("Lorem ipsom", 3000, ""),
+  Product("Flutter Developer", double.infinity, ""),
+  Product("Lorem ipsom", 3000, ""),
+  Product("Flutter Developer", double.infinity, ""),
+  Product("Lorem ipsom", 3000, ""),
+  Product("Flutter Developer", double.infinity, ""),
+  Product("Lorem ipsom", 3000, ""),
+  Product("Flutter Developer", double.infinity, ""),
 ];
